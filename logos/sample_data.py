8dc2f8c3037f885b134ea59df3aab6faadc210e6
Jinu Lee <jinulee.v@gmail.com>
2023-11-09 03:34:33 +0000

    Remove test codes and finalize sample_data.py for now

--- logos/sample_data.py
+++ logos/sample_data.py
@@ -9,12 +9,12 @@ from chain.convert_doc_to_asp import convert_doc_to_asp
 file_dir = "data/precedent_traffic_crimes.jsonl"
 target_casename = [
     "특정범죄가중처벌등에관한법률위반(도주치상)",
-    # "도로교통법위반(음주측정거부)",
-    # "도로교통법위반(사고후미조치)",
-    # "도로교통법위반(음주운전)",
-    # "특정범죄가중처벌등에관한법률위반(위험운전치상)",
-    # "교통사고처리특례법위반(치상)",
-    # "도로교통법위반(무면허운전)",
+    "도로교통법위반(음주측정거부)",
+    "도로교통법위반(사고후미조치)",
+    "도로교통법위반(음주운전)",
+    "특정범죄가중처벌등에관한법률위반(위험운전치상)",
+    "교통사고처리특례법위반(치상)",
+    "도로교통법위반(무면허운전)",
     # "도로교통법위반" # 업무상과실 재물손괴??
 ]
 
@@ -33,15 +33,15 @@ def main():
         case_data.extend(rawcase[:NUM_PER_CASE])
     db.close()
 
-    for case in case_data[3:4]:
+    for case in case_data:
         print(json.dumps(case, indent=4, ensure_ascii=False))
         result, program = convert_doc_to_asp(case, few_shot_n=8)
         result = result[0] # First goal only
-        # with open("logs/logos_trial_result.txt", "a") as file:
-        #     file.write("==============================\n")
-        #     file.write(f"{json.dumps(case, indent=4, ensure_ascii=False)}\n\n")
-        #     file.write(str(result) + "\n")
-        #     file.write(json.dumps(program, indent=4, ensure_ascii=False) + "\n\n")
+        with open("logs/logos_trial_result.txt", "a") as file:
+            file.write("==============================\n")
+            file.write(f"{json.dumps(case, indent=4, ensure_ascii=False)}\n\n")
+            file.write(str(result) + "\n")
+            file.write(json.dumps(program, indent=4, ensure_ascii=False) + "\n\n")
         logging.info("==============================\n")
         logging.info(f"{json.dumps(case, indent=4, ensure_ascii=False)}\n\n")
         logging.info(str(result) + "\n")
